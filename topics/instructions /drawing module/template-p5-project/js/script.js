cfcc203d6c6fec328ec0d9c8e182d4e5a362d27f
SOCOLOMBIAN <sophie7304@outlook.com>
2024-09-11 21:43:46 -0400

    i did changes on the names descrption

--- topics/instructions /drawing module/template-p5-project/js/script.js
+++ topics/instructions /drawing module/template-p5-project/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * drawing module
+ * sophie sanchez 
  * 
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!

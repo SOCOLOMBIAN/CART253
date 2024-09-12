643d13a7af091e08fe456a50776b11c8733d921b
SOCOLOMBIAN <sophie7304@outlook.com>
2024-09-11 21:39:58 -0400

    started on the drawind module

---
+++ topics/instructions /function_calls/js/process.js
@@ -0,0 +1,25 @@
+/**
+ * the blank page
+ * Sophie Sanchez 
+ * 
+ * xd
+ * try tying....
+ */
+
+"use strict";
+
+
+function setup() {
+    // will happen once at the beginning of the program 
+    createCanvas(649,430);
+
+}
+
+
+
+function draw() {
+     // every frame 
+     background(67,98,234);
+     square(34,23,200,524); 
+     
+}

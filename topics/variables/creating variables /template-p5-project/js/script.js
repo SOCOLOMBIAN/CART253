a6db86bd85f6ee0b5bbe76dd98a57d726aaef2d4
SOCOLOMBIAN <sophie7304@outlook.com>
2024-09-19 21:01:48 -0400

    drew a hole in a piece of cheese

--- topics/variables/creating variables /template-p5-project/js/script.js
+++ topics/variables/creating variables /template-p5-project/js/script.js
@@ -1,18 +1,18 @@
 /**
- * Title of Project
- * Author Name
+ * creating variables 
+ * sophie sanchez 
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * experimenting with variables 
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * creates the canvas 
 */
 function setup() {
 
+    createCanvas(480,480);
 }
 
 
@@ -20,5 +20,15 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    //the cheese
+    background(255,255,0);
+
+    //the hole 
+    push();
+    noStroke();
+    fill(0);
+    ellipse(300,400,87);
+    pop();
+      
 
 }

0fc0419de6c3caee1fe9a8087c6d7adc6ff02a92
SOCOLOMBIAN <sophie7304@outlook.com>
2024-09-27 16:56:21 -0400

    i change velocity of the moon

--- topics/assignment 2 jam1/Sanchez_Sophie_Jam1/js/script.js
+++ topics/assignment 2 jam1/Sanchez_Sophie_Jam1/js/script.js
@@ -6,32 +6,12 @@
 "use strict";
 
 let moon= {
-    
-    x: 100,
-    y: 150,
-    size: 40,
-    velocity: {
-        x: 0,
-        y: 0
-
-    },
-
-    minVelocity:{
-        x: -2,
-        y:-3
-    },
-
-    maxVelocity: {
-        x: 2,
-        y: 3
-    },
-     
-    acceleration:{
-        x: 0.025,
-        y: -0.05
-    }
-   
+    x:100,
+    y:150,
+    size: 70
 };
+   
+
 
 // i create the size of the canvas and the commands 
 
@@ -43,20 +23,11 @@ function setup() {
 function draw() { // i create a background with an image of the sky 
     background(0);
 
-     moon.velocity.x +=  moon. acceleration.x;
-     moon.velocity.y +=  moon. acceleration.y;
-    
-    
-     moon.velocity.x = constrain(moon.velocity.x,moon.minVelocity.x,moon.maxVelocity.x);
-     moon.velocity.y = constrain(moon.velocity.y,moon.minVelocity.y,moon.maxVelocity.y);
-    
-    moon.x= moon.x + moon.velocity.x;
-    moon.y= moon.y + moon.velocity.y;
-
-     //draw the moon 
+      //draw the moon 
      fill(255);
-     ellipse(moon.x,moon.y,moon.size);
-     
+     ellipse(moon.x, moon.y, moon.size);
+     moon.x= moon.x+1;
+   
      
 
 

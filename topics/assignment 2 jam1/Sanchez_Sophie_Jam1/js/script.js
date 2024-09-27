ce2403ec95ba0548efc482fbed2e7d423a2d8194
SOCOLOMBIAN <sophie7304@outlook.com>
2024-09-27 14:47:25 -0400

    made changes on the positionality of the moon on the background at the velocity

--- topics/assignment 2 jam1/Sanchez_Sophie_Jam1/js/script.js
+++ topics/assignment 2 jam1/Sanchez_Sophie_Jam1/js/script.js
@@ -10,6 +10,35 @@
 
 let skyImage= undefined;
 
+let moon= {
+    
+    x: 310,
+    y: 150,
+    size: 10,
+    velocity: {
+        x: 0,
+        y: 0,
+
+    },
+
+    minVelocity:{
+        x: 2,
+        y: 3
+    },
+
+    maxVelocity: {
+        x: 2,
+        y: 3
+    },
+     
+    acceleration:{
+        x: 0.025,
+        y: 0.025
+    }
+
+    
+}
+
 function preload() {
     skyImage= loadImage("assets/images/nightsky.jpg");
 } 
@@ -25,6 +54,24 @@ function setup() {
 // i create a background with an image of the sky 
 function draw() {
     background(skyImage);
+
+     moon.velocity.x= moon.velocity.x + moon. acceleration.x;
+     moon.velocity.y= moon.velocity.y + moon. acceleration.y;
+    
+    
+     moon.velocity.x = constrain(moon.velocity.x,moon.minVelocity.x,moon.maxVelocity.x);
+     moon.velocity.y = constrain(moon.velocity.y,moon.minVelocity.y,moon.maxVelocity.y);
+    
+    moon.x= moon.x + moon.velocity.x;
+    moon.y= moon.y + moon.velocity.y;
+
+     //draw the moon 
+     fill(255 );
+     ellipse(moon.x,moon.y,moon.size);
+     
+     
+
+
 
    
 

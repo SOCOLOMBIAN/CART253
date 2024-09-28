75987bf27234810301a655eb5e2dfc625945fbfd
SOCOLOMBIAN <sophie7304@outlook.com>
2024-09-28 14:57:58 -0400

    i did some changes on the code, i change the position of the circle on top of the moon to create a semi-moon

--- topics/assignment 2 jam1/Sanchez_Sophie_Jam1/js/script.js
+++ topics/assignment 2 jam1/Sanchez_Sophie_Jam1/js/script.js
@@ -5,34 +5,43 @@
 
 "use strict";
 
-let moon= {
+let moon= { // giving the moon the different variables to move around 
     x:100,
     y:150,
-    size: 70
+    size: 85
 };
-   
+
+let movingCircle= { // creating the variables 
+
+    x: 105,
+    y: 145,
+    size:75
+};
 
 
 // i create the size of the canvas and the commands 
 
 function setup() {
    createCanvas (700,600);
-   
 }
 
-function draw() { // i create a background with an image of the sky 
+function draw() { // i create a background with the color of the dark sky 
     background(0);
 
-      //draw the moon 
-     fill(255);
-     ellipse(moon.x, moon.y, moon.size);
-     moon.x= moon.x+1;
-   
-     
+    //draw the moon 
+    fill(255);
+    ellipse(moon.x, moon.y, moon.size);
+    moon.x= moon.x+1;
+    
+     drawCircle();
 
+    function drawCircle() { // call the function to move the circle moving 
+    
+    // draw the circle on top of the moon to create a semi-moon 
+    fill(0);
+    ellipse(movingCircle.x,movingCircle.y,movingCircle.size);
+    movingCircle.x= movingCircle.x +1;
+       }
+    
+};
 
-
-   
-
-
-}

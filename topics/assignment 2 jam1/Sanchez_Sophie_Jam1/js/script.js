fe264de3ab7ca20ab012d9a84cc054b1b807d7c2
SOCOLOMBIAN <sophie7304@outlook.com>
2024-09-30 16:54:40 -0400

    made changes on the position of the moon and the black cirlce

--- topics/assignment 2 jam1/Sanchez_Sophie_Jam1/js/script.js
+++ topics/assignment 2 jam1/Sanchez_Sophie_Jam1/js/script.js
@@ -18,20 +18,20 @@ velocity: {
 
 let movingCircle= { // creating the variables 
 
-    x: 105,
+    x: 110,
     y: 145,
     size:70,
 
-    velocity:{
-    x:0.5,
-    y:0,
-    }
-};
+ velocity: {
+    x: 0.5,
+    y:0
+ }
 
-// i create the size of the canvas and the commands 
+};
 
 function setup() {
    createCanvas (700,600);
+     
 }
 
 function draw() { // i create a background with the color of the dark sky 
@@ -40,33 +40,22 @@ function draw() { // i create a background with the color of the dark sky
     //draw the moon 
     fill(255);
     ellipse(moon.x, moon.y, moon.size);
-   
     moon.x+= moon.velocity.x;
 
     if (moon.x> width + moon.size) {
         moon.x = -moon.size; // reset the position of the moon
         moon.velocity.x += 0.5; // increment velocity of the moon, when reset
-
     }
 
-    drawCircle(); //call the function 
-
+    drawCircle(); // draw the moving circle 
+    
 }
 
-    function drawCircle() { // funciton circle moving 
-    
-    // draw the circle on top of the moon to create a semi-moon 
-    fill(0);
+    function drawCircle() { // funciton circle moving  
+    fill(0); // color of the circle 
     ellipse(movingCircle.x,movingCircle.y,movingCircle.size);
+    movingCircle.x += 0.5;
 
-    movingCircle.x +=movingCircle.velocity.x;
-
-    if (movingCircle.x > width + movingCircle.x) {
-        movingCircle.x = -movingCircle.size;
-        movingCircle.velocity.x += 0.5;
-        movingCircle.x+= 5;
-    
-    }
     
 }
 

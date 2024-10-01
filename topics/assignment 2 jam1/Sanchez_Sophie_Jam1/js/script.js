e6dd8be76699c19655c4e5f866d1dc55536bf97d
SOCOLOMBIAN <sophie7304@outlook.com>
2024-10-01 10:19:15 -0400

    created the mountains and change the color

--- topics/assignment 2 jam1/Sanchez_Sophie_Jam1/js/script.js
+++ topics/assignment 2 jam1/Sanchez_Sophie_Jam1/js/script.js
@@ -45,13 +45,28 @@ function draw() { // i create a background with the color of the dark sky
 
     // draw the mountains for decoration of the canvas
 
-    fill(91,71,110);
+    fill(108,47,47);
     strokeWeight(0);
-    triangle(360,130,760,500,-40,500);
+    triangle(600,195,1000,600,-100,400);
 
-    fill()
+    fill(70,8,14);
+    strokeWeight(0);
+    triangle(320,210,520,500,-300,500);
+
+// ground 
+
+  fill(60,145,57);
+  ellipse(60,640,1000,400);
+
+  fill(69,168,66);
+  ellipse(540,690,1000,400);
+
+   drawMoon();
+   drawCircle(); // draw the moving circle 
+}
 
 
+   function drawMoon() {
 
     //draw the moon 
     fill(255);
@@ -68,11 +83,8 @@ function draw() { // i create a background with the color of the dark sky
         moon.velocity.isMoving= false;
 
     }
-
-    drawCircle(); // draw the moving circle 
    
-}
-    
+} 
     function drawCircle() { // funciton circle moving  
     fill(0); // color of the circle 
     ellipse(movingCircle.x,movingCircle.y,movingCircle.size);

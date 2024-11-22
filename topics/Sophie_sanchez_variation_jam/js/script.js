44db5dada549263c14651869ec36bacdc2638baf
SOCOLOMBIAN <sophie7304@outlook.com>
2024-11-22 12:35:40 -0500

    made changes, i added a score systmen for the game if time is out the game is over

--- topics/Sophie_sanchez_variation_jam/js/script.js
+++ topics/Sophie_sanchez_variation_jam/js/script.js
@@ -1,182 +1,79 @@
 /**
- * Frogfrogfrog
- * Pippin Barr
+ * Variation Jam 
+ * Sophie Sanchez 
  * 
- * A game of catching flies with your frog-tongue
+ * a game that interacts with the brain capability 
  * 
  * Instructions:
- * - Move the frog with your mouse
- * - Click to launch the tongue
- * - Catch flies
- * 
+ * - 
+ * - 
+ * - 
  * Made with p5
  * https://p5js.org/
  */
 
 "use strict";
+let currentTime= true;
+let milliseconds=0;
+let seconds= 0;
+let gameOver= false;
+let gameWin=false;
 
-// Our frog
-const frog = {
-    // The frog's body has a position and size
-    body: {
-        x: 320,
-        y: 520,
-        size: 150
-    },
-    // The frog's tongue has a position, size, speed, and state
-    tongue: {
-        x: undefined,
-        y: 480,
-        size: 20,
-        speed: 20,
-        // Determines how the tongue moves each frame
-        state: "idle" // State can be: idle, outbound, inbound
-    }
-};
-
-// Our fly
-// Has a position, size, and speed of horizontal movement
-const fly = {
-    x: 0,
-    y: 200, // Will be random
-    size: 10,
-    speed: 3
-};
-
-/**
- * Creates the canvas and initializes the fly
- */
 function setup() {
-    createCanvas(640, 480);
+    createCanvas(700, 650);
+   
+   
 
-    // Give the fly its first random position
-    resetFly();
 }
 
 function draw() {
-    background("#87ceeb");
-    moveFly();
-    drawFly();
-    moveFrog();
-    moveTongue();
-    drawFrog();
-    checkTongueFlyOverlap();
-}
 
-/**
- * Moves the fly according to its speed
- * Resets the fly if it gets all the way to the right
- */
-function moveFly() {
-    // Move the fly
-    fly.x += fly.speed;
-    // Handle the fly going off the canvas
-    if (fly.x > width) {
-        resetFly();
+//change the color of the background depending on the game status 
+    if (gameOver){
+        background("#FF0000");
+    } else { 
+        background("#020203")
     }
+    
+    // instructions of the game
+    gameInstructions();
+
+    if (gameOver){
+        displayGameOver();
+    }
+
+
 }
 
-/**
- * Draws the fly as a black circle
- */
-function drawFly() {
-    push();
+/* draw the funvtion for the set timer on the pages*/
+
+function  gameInstructions(){
+
     noStroke();
-    fill("#000000");
-    ellipse(fly.x, fly.y, fly.size);
-    pop();
-}
+    fill("#ffffff");
+    textSize(30);
+ 
+    milliseconds= millis();
+    seconds= milliseconds/1000;
+    
+    currentTime = max(0, 5 - seconds);
+    text(int(currentTime),600,150);
 
-/**
- * Resets the fly to the left with a random y
- */
-function resetFly() {
-    fly.x = 0;
-    fly.y = random(0, 300);
-}
-
-/**
- * Moves the frog to the mouse position on x
- */
-function moveFrog() {
-    frog.body.x = mouseX;
-}
-
-/**
- * Handles moving the tongue based on its state
- */
-function moveTongue() {
-    // Tongue matches the frog's x
-    frog.tongue.x = frog.body.x;
-    // If the tongue is idle, it doesn't do anything
-    if (frog.tongue.state === "idle") {
-        // Do nothing
+    if (currentTime==0){
+        gameOver=true;
     }
-    // If the tongue is outbound, it moves up
-    else if (frog.tongue.state === "outbound") {
-        frog.tongue.y += -frog.tongue.speed;
-        // The tongue bounces back if it hits the top
-        if (frog.tongue.y <= 0) {
-            frog.tongue.state = "inbound";
-        }
-    }
-    // If the tongue is inbound, it moves down
-    else if (frog.tongue.state === "inbound") {
-        frog.tongue.y += frog.tongue.speed;
-        // The tongue stops if it hits the bottom
-        if (frog.tongue.y >= height) {
-            frog.tongue.state = "idle";
-        }
-    }
+
 }
 
-/**
- * Displays the tongue (tip and line connection) and the frog (body)
- */
-function drawFrog() {
-    // Draw the tongue tip
+function displayGameOver(){
     push();
-    fill("#ff0000");
-    noStroke();
-    ellipse(frog.tongue.x, frog.tongue.y, frog.tongue.size);
-    pop();
-
-    // Draw the rest of the tongue
-    push();
-    stroke("#ff0000");
-    strokeWeight(frog.tongue.size);
-    line(frog.tongue.x, frog.tongue.y, frog.body.x, frog.body.y);
-    pop();
-
-    // Draw the frog's body
-    push();
-    fill("#00ff00");
-    noStroke();
-    ellipse(frog.body.x, frog.body.y, frog.body.size);
+    textAlign(CENTER,CENTER);
+    textSize(24);
+    text("GAME OVER", width/2,height/2 -24);
+    text("Press R to play again", width/2, height/2 +30);
     pop();
 }
 
-/**
- * Handles the tongue overlapping the fly
- */
-function checkTongueFlyOverlap() {
-    // Get distance from tongue to fly
-    const d = dist(frog.tongue.x, frog.tongue.y, fly.x, fly.y);
-    // Check if it's an overlap
-    const eaten = (d < frog.tongue.size/2 + fly.size/2);
-    if (eaten) {
-        // Reset the fly
-        resetFly();
-        // Bring back the tongue
-        frog.tongue.state = "inbound";
-    }
-}
 
-/**
- * Launch the tongue on click (if it's not launched yet)
- */
-function mousePressed() {
-    if (frog.tongue.state === "idle") {
-        frog.tongue.state = "outbound";
-    }
-}
+
+

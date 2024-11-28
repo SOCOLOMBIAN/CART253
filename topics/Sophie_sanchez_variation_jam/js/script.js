4fe26169b7bb3f6f2d5273f52d346eee897b5861
SOCOLOMBIAN <sophie7304@outlook.com>
2024-11-28 15:52:00 -0500

    added a message for the introduction, started to work on the typing text and the game started

--- topics/Sophie_sanchez_variation_jam/js/script.js
+++ topics/Sophie_sanchez_variation_jam/js/script.js
@@ -18,48 +18,82 @@ let milliseconds=0;
 let seconds=0;
 let gameOver= false;
 let gameWin=false;
+let gameStarted= false;
 
-let message =' Welcome to the magic world of sounnd and cor, you will be asked to overcome challenges, win and see what you acomplish.'
+let eye;
 
+
+let string =' Welcome to the magic world of sounnd and color, you will be asked to overcome challenges, win and see what you acomplish.';
+let currentCharacter= 0;
+
+function preload(){
+
+     eye= loadImage("assets/images/ojo.jpg");
+}
 
 function setup() {
     createCanvas(700, 650);
-
-     
+    
 }
 
 function draw() {
 
 //change the color of the background depending on the game status 
-   if (gameOver) {
-    background("#FF0000");
-   } else{
-    background("#020203");
-   }
 
-    // update game timer
-    gameInstructions();
+if (!gameStarted){
+    background(0,0,0);
+    displayInstructions();
+}
+else if (!gameOver) {
+    background(0,0,0);
     gameTimer();
-    
-    if (gameOver){
-        displayGameOver();
+   }
+else { 
+    background(eye);
+    displayGameOver();
 
     }
 
 }
 
+       
+/* draw the function for the instruction page*/
+function  displayInstructions() {
 
-function  gameInstructions() {
+    let currentString= string.substring(0,currentCharacter);
 
+    push();
+    textSize(20);
+    textFont('courier');
+    textAlign(CENTER,CENTER);
+    fill(255);
+    text(currentString, width/2, height/2);
 
+    //condition to start the game
+    if (currentCharacter >= string.length) {
+        textSize(16);
+        text('Press SPACE to start the game',width/2,height/2 +24);
+    }
+    pop();
+
+    currentCharacter+= 0.1;
+    currentCharacter= min(currentCharacter,string.length);
 }
+
+function keyPressed(){
+
+    if (!gameStarted && key === '' && currentCharacter >= string.length);
+    gameStarted= true;
+}
+
+
        
 /* draw the function for the set timer on the pages*/
 
 function  gameTimer(){
 
     noStroke();
-    fill("#ffffff");
+    fill(255,0,0);
     textSize(30);
 
     milliseconds= millis();
@@ -76,11 +110,15 @@ function  gameTimer(){
 }
 
 function displayGameOver(){
-
+     
+    push();
     textAlign(CENTER);
+    fill(255,0,0);
     textSize(20);
+    textFont('courier');
     text('GAME OVER, KEEP TRYING',width/2,height/2 );
     text('Press any key to try again',width/2,height/2 +40);
+    Pop();
 }
 
 
